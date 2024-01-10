67d619a2cbf52c309bb2e6682c61591ccd7dbdc9
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2024-01-10 20:18:56 -0300

    refactor: Updates the code to have default hello world

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Filipe")
-	expected := "Hello, Filipe!"
-
-	if result != expected {
-		t.Errorf("Result '%s', expected '%s'", result, expected)
+	assertCorrectMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("Result '%s', expected '%s'", result, expected)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		result := Hello("Filipe")
+		expected := "Hello, Filipe!"
+		assertCorrectMessage(t, result, expected)
+	})
+
+	t.Run("saying default hello with empty string", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, world!"
+		assertCorrectMessage(t, result, expected)
+	})
 }
