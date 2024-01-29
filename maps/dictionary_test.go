ec76de9ed632a06e89d67cfa255ec6fd220e001b
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2024-01-28 23:19:47 -0300

    feat: Implent a CRUD with map

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -36,14 +36,69 @@ func TestSearch(t *testing.T) {
 	})
 }
 
-func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
-	got, err := dictionary.Search("test")
+func assertDefinition(t *testing.T, d Dictionary, word, definition string) {
+	t.Helper()
 
+	got, err := d.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
 
-	assertStrings(t, got, "this is just a test")
+	assertStrings(t, got, definition)
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordAlreadyExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		newDefinition := "new definition"
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "this is just a test"}
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("expect %q to be deleted", word)
+	}
 }
